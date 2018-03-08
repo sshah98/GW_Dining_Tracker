dc50d6d5b0a30aa66b783050a5721e6a1a1c8a2d
sshah98 <shahsuraj261@gmail.com>
2018-03-08 18:33:48 -0500

    converted file to a class based program

--- gworld_analysis.py
+++ gworld_analysis.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import Select
 # from selenium.common.exceptions import NoSuchElementException
@@ -15,144 +17,138 @@ import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
-
 import datetime as dt
 
 from sklearn.linear_model import LinearRegression
-
 from scipy.stats import linregress
 
 plt.style.use('fivethirtyeight')
 
+myuser = input('Enter username: ')
+mypass = getpass.getpass('Enter password: ')
+baseurl = "https://get.cbord.com/gwu/full/login.php"
 
-def login():
+class DiningDollars(object):
 
-    myuser = input('Enter username: ')
-    mypass = getpass.getpass('Enter password: ')
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
 
-    baseurl = "https://get.cbord.com/gwu/full/login.php"
+    def loginToAccount(self):
+        options = Options()
+        options.add_argument('--headless')
+        # Last I checked this was necessary.
+        options.add_argument('--disable-gpu')
+        mydriver = webdriver.Chrome(
+            executable_path='/usr/local/bin/chromedriver', chrome_options=options)
+        mydriver.get(baseurl)
 
-    mydriver = webdriver.Firefox()
-    mydriver.get(baseurl)
+        username = mydriver.find_element_by_id("login_username_text")
+        password = mydriver.find_element_by_id("login_password_text")
 
-    # clearuser = mydriver.find_element_by_id("login_username_text").clear()
-    # clearpass = mydriver.find_element_by_id("login_password_text").clear()
+        username.send_keys(myuser)
+        password.send_keys(mypass)
 
-    # TODO: Debug if password or username is not correct:
+        mydriver.find_element_by_name("submit").click()
+        mydriver.get("https://get.cbord.com/gwu/full/history.php")
 
-    username = mydriver.find_element_by_id("login_username_text")
-    password = mydriver.find_element_by_id("login_password_text")
+        html = mydriver.page_source
+        soup = BeautifulSoup(html, "lxml")
 
-    username.send_keys(myuser)
-    password.send_keys(mypass)
+        mydriver.close()
 
-    mydriver.find_element_by_name("submit").click()
+        return soup
 
-    mydriver.get("https://get.cbord.com/gwu/full/history.php")
+    def htmlToDataFrame(self):
+        soup = self.loginToAccount()
 
-    html = mydriver.page_source
-    soup = BeautifulSoup(html, "lxml")
+        table = soup.find(
+            "table", {"class": "table table-striped table-bordered"})
 
-    mydriver.close()
+        # adds all table rows
+        rows = list()
+        for row in table.findAll("tr"):
+            rows.append(row)
 
-    # used pickle for debugging while getting table and converting to dataframe
-    # with open('site.pickle', 'wb') as handle:
-    #     pickle.dump(soup, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    # with open('site.pickle', 'rb') as handle:
-    #     b = pickle.load(handle)
+        # removes commas and more necessary formatting
+        items = list(table.stripped_strings)
+        items = [elem for elem in items if elem.strip(",")]
 
-    table = soup.find("table", {"class": "table table-striped table-bordered"})
+        # gets after 4, because the first four are the headers - Cash, Datetime, Price, Vendor
+        items = items[4:]
+        items = [items[i:i + 5] for i in range(0, len(items), 5)]
 
-    # adds all table rows
-    rows = list()
-    for row in table.findAll("tr"):
-        rows.append(row)
+        # create a dataframe
+        df = pd.DataFrame(items)
 
-    # removes commas and more necessary formatting
-    items = list(table.stripped_strings)
-    items = [elem for elem in items if elem.strip(",")]
+        # converts Dining Cash to string instead of object
+        df[0] = df[0].astype(str)
 
-    # gets after 4, because the first four are the headers - Cash, Datetime, Price, Vendor
-    items = items[4:]
-    items = [items[i:i + 5] for i in range(0, len(items), 5)]
+        # fix prices (removes -, $) and converts to float
+        df[4] = df[4].str.replace('$', '')
+        df[4] = df[4].str.replace('- ', '')
+        df[4] = df[4].str.replace('+', '')
+        df[4] = df[4].astype(float)
 
-    # create a dataframe
-    df = pd.DataFrame(items)
+        # converts day, month, year to proper date format
+        df[1] = pd.to_datetime(df[1], errors='coerce')
 
-    # converts Dining Cash to string instead of object
-    df[0] = df[0].astype(str)
+        # converts time to proper time format
+        df[2] = pd.to_datetime(df[2])
+        df[2] = df[2] = pd.Series([val.time() for val in df[2]])
 
-    # fix prices (removes -, $) and converts to float
-    df[4] = df[4].str.replace('$', '')
-    df[4] = df[4].str.replace('- ', '')
-    df[4] = df[4].str.replace('+', '')
-    df[4] = df[4].astype(float)
+        # converts place to string
+        df[3] = df[3].astype(str)
 
-    # converts day, month, year to proper date format
-    df[1] = pd.to_datetime(df[1], errors='coerce')
+        df = df.rename(columns={0: 'Account', 1: 'Date',
+                                2: 'Time', 3: 'Vendor', 4: 'Price'})
 
-    # converts time to proper time format
-    df[2] = pd.to_datetime(df[2])
-    df[2] = df[2] = pd.Series([val.time() for val in df[2]])
+        myfile = '%s' % myuser.strip('@gwmail.gwu.edu') + '_gworld_dollars.csv'
+        df.to_csv(myfile)
 
-    # converts place to string
-    df[3] = df[3].astype(str)
+    def analysis(self):
 
-    df = df.rename(columns={0: 'Account', 1: 'Date',
-                            2: 'Time', 3: 'Vendor', 4: 'Price'})
+        df = pd.read_csv('suraj98_gworld_dollars.csv', parse_dates=['Date'])
+        df = df.sort_values(by='Date')
+        df.set_index('Date', inplace=True)
 
-    myfile = '%s' % myuser.strip('@gwu.edu') + '_gworld_dollars.csv'
-    df.to_csv(myfile)
+        df1 = df.truncate(before='2018-01-06')
+        df1['CurrentVal'] = 0
+        # df1['Current Value'] = df1['Price'] - df1['Current Value']
+        df1.CurrentVal = 1350 * 2 - df1.Price.cumsum()
 
+        df1.rename(columns={'Unnamed: 0': 'Count'}, inplace=True)
+        del df1['Count']
 
-# login()
+        plt.figure()
 
-def analysis():
+        x = df1.index
+        y = df1['CurrentVal']
 
-    # read csv file and format for proper usage
-    df = pd.read_csv('krood20@gwmail_gworld_dollars.csv', parse_dates=['Date'])
-    df = df.sort_values(by='Date')
-    df.set_index('Date', inplace=True)
+        plt.plot(x, y, 'ko')
 
-    df1 = df.truncate(before='2018-01-06')
-    df1['CurrentVal'] = 0
-    # df1['Current Value'] = df1['Price'] - df1['Current Value']
-    df1.CurrentVal = 1350 * 2 - df1.Price.cumsum()
+        df1.index = pd.to_datetime(df1.index)
+        df1.index = df1.index.map(dt.datetime.toordinal)
 
-    df1.rename(columns={'Unnamed: 0': 'Count'}, inplace=True)
-    del df1['Count']
+        x1 = df1.index
+        m, b = np.polyfit(x1, y, 1)
 
-    print(df1.head())
+        plt.plot(x1, m * x1 + b, '-')
 
-    plt.figure()
+        predicted_date = int(-b / m)
+        predicted_date = dt.datetime.fromordinal(predicted_date).date()
+        print("Predicted to run out: " + str(predicted_date))
 
-    x = df1.index
-    y = df1['CurrentVal']
+        plt.ylim(ymin=0)
+        plt.xticks(rotation='vertical')
+        plt.xlabel('Date')
+        plt.ylabel('CurrentVal')
 
-    plt.plot(x, y, 'ko')
+        plt.show()
 
-    df1.index = pd.to_datetime(df1.index)
-    df1.index = df1.index.map(dt.datetime.toordinal)
-
-    x1 = df1.index
-    m, b = np.polyfit(x1, y, 1)
-    
-    print(m)
-    
-    plt.plot(x1, m * x1 + b, '-')
-    
-    predicted_date = int(-b / m)
-    predicted_date = dt.datetime.fromordinal(predicted_date).date()
-    print("Predicted to run out: " + str(predicted_date))
-
-    plt.ylim(ymin=0)
-    plt.xticks(rotation='vertical')
-    plt.xlabel('Date')
-    plt.ylabel('CurrentVal')
-
-    # plt.show()    
-
-    plt.savefig('kyle_spending.png', bbox_inches='tight')
+        # plt.savefig('kyle_spending.png', bbox_inches='tight')
 
 
-analysis()
+myobj = DiningDollars(myuser, mypass)
+myobj.htmlToDataFrame()
+
