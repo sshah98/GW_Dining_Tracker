23395a6e0555983cca2dc22b65e8b0f2ae25cd49
sshah98 <shahsuraj261@gmail.com>
2018-06-13 11:42:02 -0400

    fixed password hashing

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, url_for, render_template, request, redirect, session, Markup
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import exc
-
 import os
 import json
 import plotly
+import hashlib
 import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 
+from flask import Flask, url_for, render_template, request, redirect, session, Markup
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
+
 from stats import graphed_spending
 from spending import Spending_History
-
-import hashlib
 
 
 APPNAME = 'GWorld Spending'
@@ -21,11 +20,13 @@ app = Flask(__name__)
 
 app.config.update(APPNAME=APPNAME,)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 from models import *
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -38,8 +39,6 @@ def home():
             name = session['email'].split("@")[0]
 
         # TODO: Implement a loading bar in html page
-        
-        # Hash the inputted password. If the same, then login 
         
         myobj = Spending_History(session['email'], session['password'])
         df = myobj.webpage_to_dataframe()
@@ -74,7 +73,6 @@ def spending_history():
         return render_template('spending_history.html', graphJSON=graphJSON)
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # login form
@@ -82,15 +80,17 @@ def login():
         return render_template('login.html')
     else:
         _email = request.form['email']
-        _pass = request.form['password']
+        _pass = hashlib.md5(request.form['password'].encode())
+        _pass = _pass.hexdigest()
+
         try:
-            
+
             # hash the password. if the same, then login
             data = User.query.filter_by(email=_email, password=_pass).first()
             if data is not None:
                 session['logged_in'] = True
-                session['email'] = _email
-                session['password'] = _pass
+                session['email'] = request.form['email']
+                session['password'] = request.form['password']
                 return redirect(url_for('home'))
             else:
                 return render_template("userError.html")
@@ -103,18 +103,17 @@ def register():
     # registration form
     try:
         if request.method == 'POST':
-            
+
             password = hashlib.md5(request.form['password'].encode())
-            
-            # TODO: make sure that different passwords are used, maybe for security
-            
+            password = password.hexdigest()
             new_user = User(fname=request.form['fname'], lname=request.form['lname'],
-                            email=request.form['email'], password=password.hexdigest())
+                            email=request.form['email'], password=password)
+
             db.session.add(new_user)
             db.session.commit()
             session['user'] = request.form['fname']
-
             return render_template('index.html')
+
     except exc.IntegrityError:
         return render_template("userError.html")
 
