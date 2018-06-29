082c0985c8f43752dc49bdd8a8770713d287448c
sshah98 <shahsuraj261@gmail.com>
2018-06-29 15:41:51 -0400

    modified sign in template working on making everything mobile friendly

--- app.py
+++ app.py
@@ -2,111 +2,117 @@
 import json
 import sys
 import os
+import hashlib
 import plotly
 import psycopg2
 import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 
-
-from scripts import tabledef
-from scripts import forms
-from scripts import helpers
 from flask import Flask, redirect, url_for, render_template, request, session, flash, Markup
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
 from flask_socketio import SocketIO, emit
 
 from spending import SpendingHistory
 
 app = Flask(__name__)
-app.secret_key = 'random-key'  # Generic key for dev purposes only
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 socketio = SocketIO(app)
-DATABASE_URL = os.environ['DATABASE_URL']
-database = psycopg2.connect(DATABASE_URL, sslmode='allow')
+database = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='allow')
 
+db = SQLAlchemy(app)
+
+from models import *
 
 # ======== Routing =========================================================== #
-# -------- Login ------------------------------------------------------------- #
+
+
 @app.route('/', methods=['GET', 'POST'])
-def login():
+def home():
+
     if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = request.form['password']
-            if form.validate():
-                if helpers.credentials_valid(username, password):
-                    session['logged_in'] = True
-                    session['username'] = username
-                    session['password'] = request.form['password']
-                    return json.dumps({'status': 'Login successful'})
-                return json.dumps({'status': 'Invalid user/pass'})
-            return json.dumps({'status': 'Both fields required'})
-        return render_template('login.html', form=form)
-    user = helpers.get_user()
-    session['email'] = user.email
-    
-    df = pd.read_sql_query("SELECT * FROM history WHERE email='%s'" % (session['email']), database)
-        
-    if df.empty:
-        return redirect(url_for('refresh'))
-    
+        return render_template('login.html')
     else:
-        df['currentval'] = np.nan
-        df['currentval'] = df['price'] - df['currentval']
-        df.currentval = 1350 + df.price.cumsum()
-        df.drop(columns=['date', 'time'], inplace=True)
-        df.set_index('datetime', inplace=True)
-        print(df)
-        graphs = 0
-        return render_template('home.html', user=user, graphs=graphs)
+        if 'email' in session:
+            name = session['email'].split("@")[0]
+
+            # df = pd.read_sql_query(
+            #     "SELECT * FROM history WHERE email='%s'" % (session['email']), database)
+            # 
+            # if df.empty:
+            #     return redirect(url_for('refresh'))
+            # 
+            # else:
+            #     df['currentval'] = np.nan
+            #     df['currentval'] = df['price'] - df['currentval']
+            #     df.currentval = 1350 + df.price.cumsum()
+            #     df.drop(columns=['date', 'time'], inplace=True)
+            #     df.set_index('datetime', inplace=True)
+
+            graphs = 0
+            
+            return render_template('home.html', user=name, graphs=graphs)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # login form
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        _email = request.form['email']
+        _pass = hashlib.md5(request.form['pass'].encode())
+        _pass = _pass.hexdigest()
+
+        print(_email, _pass, request.form['email'], request.form['pass'])
+
+        try:
+
+            # hash the password. if the same, then login
+            data = User.query.filter_by(email=_email, password=_pass).first()
+
+            if data is not None:
+                session['logged_in'] = True
+                session['email'] = request.form['email']
+                session['pass'] = request.form['pass']
+
+                return redirect(url_for('home'))
+            else:
+                flash("Try again")
+                return render_template('login.html')
+        except:
+            flash("Some error")
+            return render_template("login.html")
 
 
-
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return redirect(url_for('login'))
-
-
-# -------- Signup ---------------------------------------------------------- #
 @app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = helpers.hash_password(request.form['password'])
-            email = request.form['email']
-            if form.validate():
-                if not helpers.username_taken(username):
-                    helpers.add_user(username, password, email)
-                    session['logged_in'] = True
-                    session['username'] = username
-                    session['email'] = request.form['email']
-                    session['password'] = request.form['password']
-                    return json.dumps({'status': 'Signup successful'})
-                return json.dumps({'status': 'Username taken'})
-            return json.dumps({'status': 'User/Pass required'})
-        return render_template('login.html', form=form)
-    return redirect(url_for('login'))
+def register():
+    # registration form
+    if request.method == 'GET':
+        return render_template('signup.html')
 
-
-# -------- Settings ---------------------------------------------------------- #
-@app.route('/settings', methods=['GET', 'POST'])
-def settings():
-    if session.get('logged_in'):
+    try:
         if request.method == 'POST':
-            password = request.form['password']
-            if password != "":
-                password = helpers.hash_password(password)
-            email = request.form['email']
-            helpers.change_user(password=password, email=email)
-            return json.dumps({'status': 'Saved'})
-        user = helpers.get_user()
-        return render_template('settings.html', user=user)
-    return redirect(url_for('login'))
+
+            password = hashlib.md5(request.form['pass'].encode())
+            hashed_pass = password.hexdigest()
+            new_user = User(
+                name=request.form['name'], email=request.form['email'], password=hashed_pass)
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['email'] = request.form['email']
+            session['pass'] = request.form['pass']
+
+            flash("please login now")
+            return render_template('login.html')
+
+    except exc.IntegrityError:
+        flash("Error")
+        return render_template("signup.html")
 
 
 @app.route('/info', methods=['GET', 'POST'])
@@ -120,12 +126,12 @@ def info():
 @app.route('/refresh', methods=['GET', 'POST'])
 def refresh():
 
-    SpendingHistory(session['email'], session['password']).spending_history()
+    SpendingHistory(session['email'], session['pass']).spending_history()
     # print(session['spending'])
 
     flash('GWorld Dining Dollars Data Updated!')
 
-    return render_template('home.html')
+    return redirect(url_for('home'))
 
 
 @app.route('/spending_graph', methods=['GET', 'POST'])
@@ -165,10 +171,37 @@ def spending_history():
         return render_template('spending_history.html', graphJSON=graphJSON)
 
 
+@app.route('/logout/')
+def logout():
+    # logout form
+    session['logged_in'] = False
+    return redirect(url_for('home'))
+
+
 @socketio.on('disconnect')
 def disconnect_user():
     session.clear()
     session.pop('random-key', None)
+
+
+#
+#
+# # -------- Settings ---------------------------------------------------------- #
+# @app.route('/settings', methods=['GET', 'POST'])
+# def settings():
+#     if session.get('logged_in'):
+#         if request.method == 'POST':
+#             password = request.form['password']
+#             if password != "":
+#                 password = helpers.hash_password(password)
+#             email = request.form['email']
+#             helpers.change_user(password=password, email=email)
+#             return json.dumps({'status': 'Saved'})
+#         user = helpers.get_user()
+#         return render_template('settings.html', user=user)
+#     return redirect(url_for('login'))
+#
+#
 
 
 # ======== Main ============================================================== #
---
+++ models.py
@@ -0,0 +1,29 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email = db.Column(db.String(), unique=True)
+    password = db.Column(db.String())
+
+    def __repr__(self):
+        return 'Email {}>'.format(self.email)
+
+
+class History(db.Model):
+    __tablename__ = 'history'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    account = db.Column(db.Text)
+    time = db.Column(db.DateTime)
+    vendor = db.Column(db.Text)
+    price = db.Column(db.Float)
+    email = db.Column(db.String(), db.ForeignKey('users.email'))
+    datetime = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
