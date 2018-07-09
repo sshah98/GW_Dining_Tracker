059c220d19dce4b5cfa74dbe6ae9e54b99ba0e80
sshah98 <shahsuraj261@gmail.com>
2018-07-09 09:53:19 -0400

    fixed issue so now entire program is working

--- spending.py
+++ spending.py
@@ -27,7 +27,7 @@ class SpendingHistory():
             options = Options()
 
             # --- heroku --- #
-            options.binary_location = os.environ['GOOGLE_CHROME_BIN']
+            # options.binary_location = os.environ['GOOGLE_CHROME_BIN']
 
             options.add_argument('--disable-gpu')
             options.add_argument('--no-sandbox')
@@ -35,10 +35,10 @@ class SpendingHistory():
             options.add_argument("--disable-extensions")
 
             # --- local testing --- #
-            # driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
+            driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
 
             # --- heroku --- #
-            driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=options)
+            # driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=options)
 
             print("opening chrome")
 
@@ -93,10 +93,10 @@ class SpendingHistory():
                 df.sort_values(by='datetime', inplace=True, ascending=True)
 
                 # --- local testing --- #
-                # engine = create_engine("postgresql://suraj:password@localhost/gworld")
+                engine = create_engine("postgresql://suraj:password@localhost/gworld")
 
                 # --- heroku --- #
-                engine = create_engine("postgresql+psycopg2://gvbgcpweihoipq:d52e382574f9ad2313c882534fb07ceb52484e04f112b3e405c3e9ee441048b2@ec2-54-235-206-118.compute-1.amazonaws.com:5432/d4n9qk3lo1qsr2")
+                # engine = create_engine("postgresql+psycopg2://gvbgcpweihoipq:d52e382574f9ad2313c882534fb07ceb52484e04f112b3e405c3e9ee441048b2@ec2-54-235-206-118.compute-1.amazonaws.com:5432/d4n9qk3lo1qsr2")
 
                 for i in range(len(df)):
                     try:
